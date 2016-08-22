a7c1b60768a490705cff2ef92a8bbd616bcec6a2
Thomas Peyrard <Thomas.PEYRARD@murex.com>
2016-08-22 17:15:00 +0200

    Add test to make sure the sure can't play more parties than expected.

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -4,20 +4,41 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.List;
 
 import org.junit.Test;
 
+import com.google.common.base.Joiner;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.Lists;
+
 public class MainTest {
+
+   private List<String> playerHands = Lists.newArrayList("paper", "rock", "scissors");
 
    @Test
    public void
    user_can_play_n_parties() throws IOException {
-      ByteArrayInputStream threeParties = new ByteArrayInputStream("paper\nrock\nscissors".getBytes());
+      ByteArrayInputStream threeParties = new ByteArrayInputStream(Joiner.on('\n').join(playerHands).getBytes());
 
       final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(threeParties));
-      Main.playParties(3, bufferedReader);
+      Main.playParties(playerHands.size(), bufferedReader);
 
       assertThat(bufferedReader.readLine()).isNull();
+   }
+
+   @Test
+   public void
+   user_can_play_only_the_defined_number_of_parties() throws IOException {
+      final String additionalHand = "paper";
+      final String MorePlayerHands = FluentIterable.from(playerHands).append(additionalHand).join(Joiner.on('\n'));
+
+      ByteArrayInputStream threeParties = new ByteArrayInputStream(MorePlayerHands.getBytes());
+
+      final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(threeParties));
+      Main.playParties(playerHands.size(), bufferedReader);
+
+      assertThat(bufferedReader.readLine()).isEqualTo(additionalHand);
    }
 
    @Test
